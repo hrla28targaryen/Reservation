a034636adb47d8ddcd7e65296256c225de0fefae
Albert Saputra <a.albert.saputra@gmail.com>
2019-04-07 03:12:38 -0700

    [prog] callout bubble added, next will be adding CSS modal

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
 import ReactDOM from 'react-dom';
 import header from './header.scss';
 import inputForm from './form.scss';
+import info from './info.scss';
 import $ from 'jquery';
 
 class Reservation extends React.Component {
@@ -65,7 +67,12 @@ class Reservation extends React.Component {
                   </span>
                 </span>
               </span>
-              <img src="question.png" className={header.pdpPrice_noDiscount} />
+              <button className={header.questionButton} >
+              </button>
+              <span className={header.tooltiptext}>
+                <p className={header.description}>Price varies depending on date. <br/>Pick your date to see price.</p>
+                <p className={header.descExit}>x</p>
+              </span>
               <span className={header.pdpPrice_priceType} >
                 |
               </span>
@@ -121,14 +128,14 @@ class Reservation extends React.Component {
                   <div className={inputForm.reservationDateWindow_durations}>
                     <div>
                       <label className={inputForm.radioContainter}>
-                        <input type="radio" id="rentaldays-4" value="4" defaultChecked name="radio"/>
+                        <input type="radio" id="rentaldays-4" value="4" defaultChecked name="radio" />
                         <span className={inputForm.checkmark} value="4" ></span>
                         <div className={inputForm.radioWord}>4-day rental</div>
                       </label>
                     </div>
                     <div>
                       <label className={inputForm.radioContainter}>
-                        <input type="radio" id="rentaldays-8" value="8" name="radio"/>
+                        <input type="radio" id="rentaldays-8" value="8" name="radio" />
                         <span className={inputForm.checkmark} value="8" ></span>
                         <div className={inputForm.radioWord}>8-day rental</div>
                       </label>
@@ -139,16 +146,86 @@ class Reservation extends React.Component {
                     <label className={inputForm.datepickerLabel} htmlFor="holdDate"></label>
                     <input type="date" className={inputForm.datePicker} readOnly="" id="holdDate" />
                   </div>
-
                   <input type="hidden" name="reservation[date]" value="" />
                 </fieldset>
+              </div>
+
+              <div className={inputForm.ctaContainer}>
+                <div className={inputForm.pro_cta}>
+                  <div className="input-wrapper pro-cta__input-pair">
+                    <input className={inputForm.includeProHidden} type="hidden" name="includePro" value="0" />
+                    <label className={inputForm.includeProCheckbox}>
+                      <input type="checkbox" id="reservationPro" name="includePro" value="1" />
+                      <span className={inputForm.includeProSpan}></span>
+                    </label>
+                    <label htmlFor="reservationPro" className={inputForm.proLabel}>I want free shipping &amp; insurance with PRO.
+                    </label>
+                    <a className={inputForm.standAlone} type="button">Learn More</a>
+                  </div>
+                  <div className={inputForm.labelPRO}></div>
+                </div>
+              </div>
+
+              <div className="add-to-bag-button">
+              <button className={inputForm.btnFullWidth} type="submit">Add to Bag</button>
               </div>
             </div>
           </form>
         </div>
+
+        <div id="3info">
+          <div className={info.infoCallout} >
+            <div className={info.infoCalloutSurtext} >
+              100% Fit Guarantee
+            </div>
+            <div className={info.infoCalloutHed} >
+              Select your free second style on the next screen.
+            </div>
+            <p className={info.infoCalloutDek}>
+              That's&nbsp;
+              <span className={info.italic}>in addition</span>
+              &nbsp;to your free backup size.
+            </p>
+          </div>
+
+          <div className={info.productDetails}>
+            <Collapsible transitionTime="10"  trigger="Stylist Notes"className={info.collapsibleTitle}  >
+              <div className={info.collapsibleContent}>
+                <p>Experience this beautiful {this.state.productName.split(' ')[1]} designed by {this.state.designerName}.</p>
+              </div>
+            </Collapsible>
+
+            <Collapsible transitionTime="10" trigger="SIZE &amp; FIT" className={info.collapsibleTitle}  >
+              <div className={info.collapsibleContent}>
+                <p>Size available from {String(allSize[0]).split(',')[0]} to {String(allSize[0]).split(',')[0]===String(allSize[allSize.length-1]).split(',')[0]?String(allSize[allSize.length-2]).split(',')[0]:String(allSize[allSize.length-1]).split(',')[0]}</p>
+              </div>
+            </Collapsible>
+
+            <Collapsible transitionTime="10" trigger="Product Details" className={info.collapsibleTitle}  >
+              <div className={info.collapsibleContent}>
+                <p>{this.state.productName.split(' ')[0]}. See size and fit tab for length. Imported.</p>
+              </div>
+            </Collapsible>
+
+            <Collapsible transitionTime="10" trigger="Share" className={info.collapsibleTitle}  >
+              <div className={info.collapsibleContent}>
+                <div className={info.facebook}>
+                  <button> <div> </div><b>Like</b> {this.state.facebook}</button>
+                </div>
+                <div className={info.share}>
+                  <button> <b>Share</b></button>
+                </div>
+                <button className={info.pinteresta} >
+                </button>
+              </div>
+            </Collapsible>
+          </div>
+        </div>
+
       </div>
     );
   }
 };
+
 
 ReactDOM.render(<Reservation />, document.getElementById('Reservation'))
